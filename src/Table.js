d7204e0a7dab6987eaaa8f31c29e8501faa69553
Bahare <itxbb09@gmail.com>
2023-04-15 15:43:41 +0100

    Revert "connecting table and input"
    This reverts commit 628f84a614b9de87aa8a54719045cbe63aee5b8b.

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Input from "./Input";
 
 const Table = () => {
   const [data, setData] = useState([]);
@@ -18,41 +19,59 @@ const Table = () => {
       .catch((error) => console.error(error));
   };
 
+  const addCustomer = async (input) => {
+    // Send a POST request to the API with the input value
+    const response = await fetch("https://hotel-node3.onrender.com/bookings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    });
+
+    // Handle the response
+    const data = await response.json();
+    setData(data);
+  };
+
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Email</th>
-          <th>Room ID</th>
-          <th>Check in Date</th>
-          <th>Check out Date</th>
-          <th>Delete Booking</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((booking) => (
-          <tr key={booking.id}>
-            <td>{booking.id}</td>
-            <td>{booking.title}</td>
-            <td>{booking.firstName}</td>
-            <td>{booking.surname}</td>
-            <td>{booking.email}</td>
-            <td>{booking.roomId}</td>
-            <td>{booking.checkInDate}</td>
-            <td>{booking.checkOutDate}</td>
-            <td>
-              <button onClick={() => handlerDeleteClick(booking.id)}>
-                Delete
-              </button>
-            </td>
+    <>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Room ID</th>
+            <th>Check in Date</th>
+            <th>Check out Date</th>
+            <th>Delete Booking</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((booking) => (
+            <tr key={booking.id}>
+              <td>{booking.id}</td>
+              <td>{booking.title}</td>
+              <td>{booking.firstName}</td>
+              <td>{booking.surname}</td>
+              <td>{booking.email}</td>
+              <td>{booking.roomId}</td>
+              <td>{booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>
+                <button onClick={() => handlerDeleteClick(booking.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Input addCustomer={addCustomer} />
+    </>
   );
 };
 
