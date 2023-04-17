ab46878c3a1843b533c33c4760dff509707509c7
Bahare <itxbb09@gmail.com>
2023-04-17 12:28:54 +0100

    hotel logo

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,10 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <img src="logo.png" alt="Hotel Booking Logo" />
+      <img
+        src="https://cdn.vectorstock.com/i/1000x1000/18/96/hotel-icon-vector-46581896.webp"
+        alt="Hotel Booking Logo"
+      />
       <h1>Hotel Booking</h1>
     </header>
   );
