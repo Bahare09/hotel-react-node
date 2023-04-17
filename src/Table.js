ef02cadd2e633eb3b09becafaf6ab9e329a69cae
Bahare <itxbb09@gmail.com>
2023-04-17 12:18:07 +0100

    some update for Loading error

--- src/Table.js
+++ src/Table.js
@@ -4,20 +4,34 @@ import Search from "./Search";
 
 const Table = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://hotel-node3.onrender.com/bookings")
       .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
   const handlerDeleteClick = (id) => {
     fetch(`https://hotel-node3.onrender.com/bookings/${id}`, {
       method: "DELETE",
     })
       .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   const handlerRefreshClick = () => {
@@ -54,40 +68,44 @@ const Table = () => {
       <Input addCustomer={addCustomer} />
       <Search search={search} />
       <button onClick={() => handlerRefreshClick()}>Refresh Page</button>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>First Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-            <th>Room ID</th>
-            <th>Check in Date</th>
-            <th>Check out Date</th>
-            <th>Delete Booking</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((booking) => (
-            <tr key={booking.id}>
-              <td>{booking.id}</td>
-              <td>{booking.title}</td>
-              <td>{booking.firstName}</td>
-              <td>{booking.surname}</td>
-              <td>{booking.email}</td>
-              <td>{booking.roomId}</td>
-              <td>{booking.checkInDate}</td>
-              <td>{booking.checkOutDate}</td>
-              <td>
-                <button onClick={() => handlerDeleteClick(booking.id)}>
-                  Delete
-                </button>
-              </td>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>First Name</th>
+              <th>Surname</th>
+              <th>Email</th>
+              <th>Room ID</th>
+              <th>Check in Date</th>
+              <th>Check out Date</th>
+              <th>Delete Booking</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((booking) => (
+              <tr key={booking.id}>
+                <td>{booking.id}</td>
+                <td>{booking.title}</td>
+                <td>{booking.firstName}</td>
+                <td>{booking.surname}</td>
+                <td>{booking.email}</td>
+                <td>{booking.roomId}</td>
+                <td>{booking.checkInDate}</td>
+                <td>{booking.checkOutDate}</td>
+                <td>
+                  <button onClick={() => handlerDeleteClick(booking.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
